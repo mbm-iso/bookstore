28ac4b492e54cf997f18ed93554e0c736163b8d9
mbm <mohamedali.mbarek@iso-gruppe.com>
2025-06-16 09:48:12 +0200

    Code cleaning

--- src/main/java/com/example/bookstore/persistence/mapper/BookEntityMapper.java
+++ src/main/java/com/example/bookstore/persistence/mapper/BookEntityMapper.java
@@ -9,9 +9,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface BookEntityMapper {
 
